ab6838d18c800adab8dc92cdc73e445cf5729d43
Asiya <asiyaahmed33@yahoo.com>
2022-08-01 15:13:54 -0700

    deleted comments

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const inquirer = require("inquirer");
 const express = require('express');
 const mysql = require('mysql2');
 
-// const PORT = process.env.PORT || 3001;
 const app = express();
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
 
 const db = mysql.createConnection(
     {
